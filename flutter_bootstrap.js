426354911b7b2e4c273c1eec1235e4bb02a6531e
abhinash9177 <92246925+abhinash9177@users.noreply.github.com>
2024-08-08 19:20:03 +0530

    Removed export button

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2431593528"
+    serviceWorkerVersion: "2639379899"
   }
 });
