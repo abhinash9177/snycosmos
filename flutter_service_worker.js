50e773c69801774a98ed57873bd70fd84be913e7
abhinash9177 <92246925+abhinash9177@users.noreply.github.com>
2024-10-20 18:42:51 +0530

    fixed application

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "f5e86c5498f0cad3f236181279398b40",
+const RESOURCES = {"flutter_bootstrap.js": "0eeaed420f7cf8087ac296f5ad4069f0",
 "version.json": "ff1e871924222b17e6c5912aaed760fa",
 "index.html": "280be957cd48598e0b8bffb1cc07335d",
 "/": "280be957cd48598e0b8bffb1cc07335d",
-"main.dart.js": "f607a5e362f1a39fc4c40b74b965fe7b",
+"main.dart.js": "01b7d9ba5e61c8f214752e3a2ae029ce",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
