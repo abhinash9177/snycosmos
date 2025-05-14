b3ffbbaebfb5e902dfc83e197d137f04946306c7
abhinash9177 <92246925+abhinash9177@users.noreply.github.com>
2025-05-14 12:22:06 +0530

    added grouping in applications

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "32e9b33e2a80a738511bd0796322b3e9",
+const RESOURCES = {"flutter_bootstrap.js": "354a63e4631e421a69fc29194d19aa99",
 "version.json": "ff1e871924222b17e6c5912aaed760fa",
 "index.html": "280be957cd48598e0b8bffb1cc07335d",
 "/": "280be957cd48598e0b8bffb1cc07335d",
-"main.dart.js": "717ab33930fa8a0d8df9498e1aff4a73",
+"main.dart.js": "a8aaa55634119431a0a1d491efae599c",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
