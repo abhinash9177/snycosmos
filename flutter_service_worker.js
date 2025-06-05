f4cebd6d7c795ca686b50811f0ccc9922763f903
abhinash9177 <92246925+abhinash9177@users.noreply.github.com>
2025-06-05 19:46:17 +0530

    fixed navigation issues

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "354a63e4631e421a69fc29194d19aa99",
+const RESOURCES = {"flutter_bootstrap.js": "1ab2ff01012235f92e14753e200f3483",
 "version.json": "ff1e871924222b17e6c5912aaed760fa",
 "index.html": "280be957cd48598e0b8bffb1cc07335d",
 "/": "280be957cd48598e0b8bffb1cc07335d",
-"main.dart.js": "a8aaa55634119431a0a1d491efae599c",
+"main.dart.js": "c594eab9c1ef4ff9b5597178eaa9cebe",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
